1a00abc923041c39036768a3b1a07de434793999
Vovchek <Vlad.aks03@mail.ru>
2021-03-29 16:38:31 +0500

    больной скорее жив

--- data/users.py
+++ data/users.py
@@ -11,19 +11,19 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
     sex = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     age = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
-    sexual_orientation = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     about = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     au_attitude = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     frog_attitude = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     cvc_code = au_attitude = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
     # jobs = orm.relation("Job")
 
     def set_password(self, password):
